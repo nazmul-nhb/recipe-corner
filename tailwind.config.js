3bfc74563e46484a690678a476a8fff2a77cad85
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-15 20:52:48 +0600

    set up daisyUI

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,4 @@
+import daisyui from './node_modules/daisyui'
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
@@ -7,6 +8,8 @@ export default {
   theme: {
     extend: {},
   },
-  plugins: [],
+  plugins: [daisyui],
+  daisyui: {
+    themes: ['light'],
+  },
 }
-

f330c14ec5a5fcba3cf8530ee67c54053e15b18d
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-16 13:58:56 +0600

    fixed want to cook table serial number

--- src/components/Recipes/Recipes.jsx
+++ src/components/Recipes/Recipes.jsx
@@ -28,7 +28,7 @@ const Recipes = () => {
                     <p className="text-[#150B2B99]">We cook the best dishes in the continent. You are welcome here to enjoy our delicious dishes! Our dishes are so delicious that you will never forget the taste!</p>
                 </div>
                 <div className="flex flex-col lg:flex-row gap-6">
-                    <div className="w-full lg:w-2/3 grid grid-cols-1 md:grid-cols-2 gap-6">
+                    <div className="w-full lg:w-3/5 grid grid-cols-1 md:grid-cols-2 gap-6">
                         {
                             recipes.map(recipe => <Recipe
                                 key={recipe.recipe_id}
@@ -37,17 +37,17 @@ const Recipes = () => {
                             ></Recipe>)
                         }
                     </div>
-                    <div className="w-full lg:w-1/3 flex flex-col gap-8 px-6 py-8 border border-[#28282833] rounded-2xl">
+                    <div className="w-full lg:w-2/5 flex flex-col gap-8 px-6 py-8 border border-[#28282833] rounded-2xl">
                         <div className="">
                             <h3 className="text-center text-2xl font-semibold text-[#282828]">Want to Cook: {toCook.length} </h3>
                             <hr className="border border-[#28282826] w-3/4 mx-auto my-4" />
-                            <table className='fira-sans table-auto'>
-                                <thead className='text-left'>
-                                    <tr>
-                                        <th>Name</th>
-                                        <th>Time</th>
-                                        <th>Calories</th>
-                                        <th></th>
+                            <table className='text-[#878787] fira-sans table-auto'>
+                                <thead className='text-left text-base '>
+                                    <tr className="">
+                                        <th className="font-medium pr-6 pb-6"></th>
+                                        <th className="font-medium pr-6 pb-6">Name</th>
+                                        <th className="font-medium pr-6 pb-6">Time</th>
+                                        <th className="font-medium pr-6 pb-6">Calories</th>
                                     </tr>
                                 </thead>
                                 <tbody>
@@ -55,6 +55,7 @@ const Recipes = () => {
                                         toCook.map(recipe => <ToCook
                                             key={recipe.recipe_id}
                                             recipe={recipe}
+                                            serial={toCook.indexOf(recipe)}
                                         ></ToCook>)
                                     }
 
@@ -64,6 +65,7 @@ const Recipes = () => {
                         <div className="">
                             <h3 className="text-center text-2xl font-semibold text-[#282828]">Currently Cooking: </h3>
                             <hr className="border border-[#28282826] w-3/4 mx-auto my-4" />
+
                         </div>
                     </div>
                 </div>
--- src/components/ToCook/ToCook.jsx
+++ src/components/ToCook/ToCook.jsx
@@ -1,20 +1,22 @@
 import PropTypes from 'prop-types';
 
-const ToCook = ({ recipe }) => {
+const ToCook = ({ recipe, serial }) => {
     const { recipe_name, preparing_time, calories } = recipe;
     console.log(recipe);
     return (
-            <tr className='p-2'>
-                <td>{recipe_name}</td>
-                <td>{preparing_time} minutes</td>
-                <td>{calories} calories</td>
-                <td><button className="w-28 h-9 text-[#150B2B] rounded-[50px] font-medium text-base bg-[#0BE58A] border border-[#0BE58A] hover:text-[#0BE58A] hover:border-[#0BE58A] hover:bg-transparent">Preparing</button></td>
-            </tr>
+        <tr>
+            <td className='pr-6 pb-4 text-[#282828CC] font-semibold'>{serial + 1}</td>
+            <td className='pr-6 pb-4'>{recipe_name}</td>
+            <td className='pr-6 pb-4'>{preparing_time} minutes</td>
+            <td className='pr-6 pb-4'>{calories} calories</td>
+            <td><button className="w-28 h-9 text-[#150B2B] rounded-[50px] font-medium text-base bg-[#0BE58A] border border-[#0BE58A] hover:text-[#0BE58A] hover:border-[#0BE58A] hover:bg-transparent mb-6">Preparing</button></td>
+        </tr>
     );
 };
 
 ToCook.propTypes = {
-    recipe: PropTypes.object.isRequired
+    recipe: PropTypes.object.isRequired,
+    serial: PropTypes.number.isRequired
 }
 
 export default ToCook;

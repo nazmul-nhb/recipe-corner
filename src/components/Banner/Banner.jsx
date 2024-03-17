07183b2024e8243d075a9cd52a0f426843145b6e
nazmul-nhb <nazmulnhb007@yahoo.com>
2024-03-17 12:02:38 +0600

    fixed padding in top banner

--- src/components/Banner/Banner.jsx
+++ src/components/Banner/Banner.jsx
@@ -4,7 +4,7 @@ const Banner = () => {
     return (
         <div className="font-lexend mx-6 lg:mx-32 lg:mt-12 mt-6">
             <div className="max-w-[1600px] w-full h-auto mx-auto">
-                <div className="w-full h-auto flex flex-col justify-center items-center gap-4 md:gap-8 bg-cover bg-center bg-no-repeat rounded-3xl text-center px-6 py-6 md:px-20 xl:px-48 md:py-16 xl:py-32" style={{ backgroundImage: `url(${banner})` }}>
+                <div className="w-full h-auto flex flex-col justify-center items-center gap-4 md:gap-8 bg-cover bg-center bg-no-repeat rounded-3xl text-center p-6 md:p-16 xl:p-32" style={{ backgroundImage: `url(${banner})` }}>
                     <div className="w-full">
                         <h2 className="text-xl md:text-4xl lg:text-5xl text-white font-bold leading-tight md:leading-normal lg:leading-normal">Discover an exceptional cooking class tailored for you!</h2>
                     </div>
